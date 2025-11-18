e2e524c3fbbf20190f4153263d984423cb94194c
Neha Pandey <nehapandey408@gmail.com>
2025-11-19 04:33:58 +0545

    Time: 0 ms (100%), Space: 18 MB (10.8%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,17 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        ans = 1.0
-        nn = abs(n)  # Take absolute value of n
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1/x
+            n = -n
+        if n %2 == 0:
+            return self.myPow(x*x, n//2)
+        else:
+            return x* self.myPow(x, n-1)
+
         
-        while nn:
-            if nn % 2 == 1:  # If nn is odd, multiply ans by x
-                ans *= x
-                nn -= 1
-            else:
-                x *= x  # Square the base
-                nn //= 2  # Integer division
-        
-        if n < 0:  # If n is negative, take reciprocal
-            ans = 1 / ans
-        
-        return ans
