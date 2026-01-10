d74a6c41d6c441a9cae9ad751128fad91b153fb7
Neha Pandey <nehapandey408@gmail.com>
2026-01-11 05:23:35 +0545

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.py
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.py
@@ -5,33 +5,27 @@ from typing import List
 sys.setrecursionlimit(10**8)
 class Solution:
     def countDistinctIslands(self, grid : List[List[int]]) -> int:
-        rows, columns = len(grid), len(grid[0])
-        visisted =  [[False]*columns for _ in range(rows)]
-        distinct_islands =set()
-        
-        #create dfs
-        def dfs(r,c,base_r, base_c, shape):
-            # boundary check
-            if r<0 or r>=rows or c<0 or c>=columns:
+        row,coloumn = len(grid), len(grid[0])
+        visited = [[False]*coloumn for _ in range(row)]
+        unique_shape =set()
+
+        def dfs(r,c, base_r, base_c, shape):
+            # check boundary
+            if r < 0 or r>= row or c < 0 or c >= coloumn:
+                return 
+            if grid[r][c] !=1 or visited[r][c]:
                 return
-            if grid[r][c]!= 1 or visisted[r][c]:
-                return
-            visisted[r][c]= True
-            
-            #store relevent shape
-            shape.append((r-base_r, c-base_c))
-            
-            #Explore 4 directions:
-            dfs(r+1, c, base_r, base_c, shape)
-            dfs(r-1, c, base_r, base_c, shape)
-            dfs(r, c+1, base_r, base_c, shape)
-            dfs(r, c-1, base_r, base_c, shape)
-        
-        # Traverse the grid
-        for i in range(rows):
-            for j in range(columns):
-                if grid[i][j] ==1 and not visisted[i][j]:
-                    shape= []
-                    dfs(i,j, i, j,shape)
-                    distinct_islands.add(tuple(shape))
-        return len(distinct_islands)
+            visited[r][c]=True
+            shape.append((r -base_r, c-base_c))
+            dfs(r+1,c,base_r,base_c, shape)
+            dfs(r-1,c,base_r, base_c, shape)
+            dfs(r,c+1,base_r, base_c, shape)
+            dfs(r,c-1,base_r,base_c, shape)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1 and not visited[i][j]:
+                    shape = []
+                    dfs(i,j,i,j, shape)
+                    unique_shape.add(tuple(shape))
+        return len(unique_shape)
