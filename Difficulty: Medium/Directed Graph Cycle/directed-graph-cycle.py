52befcc84e7bc4f5e7bb84371c6739c13ee3f3aa
Neha Pandey <nehapandey408@gmail.com>
2026-01-12 05:09:25 +0545

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isCyclic(self, V, edges):
+        # lets build adj list
+        adj = [[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
+        
+        # lets build visited and path visited
+        visited = [False]*V
+        path_visited = [False]*V
+        
+        #step 3: create a dfs
+        # Mark node as visited
+        # Mark node as part of current path
+        # Visit neighbors
+        # If neighbor already in path → cycle found
+        # Backtrack (remove from path)
+        def dfs(node):
+            visited[node] = True
+            path_visited[node] = True
+
+            for neighbour in adj[node]:
+                if not visited[neighbour]:
+                    if dfs(neighbour):
+                        return True
+                elif path_visited[neighbour]:
+                    return True
+
+            # Backtrack
+            path_visited[node] = False
+            return False
+        
+        for i in range(V):
+            if not visited[i]:
+                if dfs(i):
+                    return True
+        return False
+            
